23aeab90c4ab4f385209a0cefae7dc65271ff057
Jee Lee <developer@jeemail.com>
2023-04-02 12:03:10 -0500

    remove pgtype module

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,16 +1,5 @@
 package helpers
 
-import (
-	"fmt"
-	"github.com/jackc/pgtype"
-)
-
-func UUIDToString(uuid pgtype.UUID) string {
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid.Bytes[0:4], uuid.Bytes[4:6], uuid.Bytes[6:8], uuid.Bytes[8:10], uuid.Bytes[10:16])
-}
-
-func StringToUUID(uuid string) pgtype.UUID {
-	var result pgtype.UUID
-	_ = (&result).Set(uuid)
-	return result
+func Pointer[T any](v T) *T {
+	return &v
 }
--- internal/repository/category.go
+++ internal/repository/category.go
@@ -3,52 +3,50 @@ package repository
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgtype"
-	pgxdecimal "github.com/jackc/pgtype/ext/shopspring-numeric"
-	"github.com/jee-lee/budget-core/internal/helpers"
+	"github.com/google/uuid"
 	"time"
 )
 
 type Category struct {
-	ID               pgtype.UUID         `db:"id"`
-	UserID           pgtype.UUID         `db:"user_id"`
-	Name             string              `db:"name"`
-	ParentCategoryID *pgtype.UUID        `db:"parent_category_id"`
-	Maximum          *pgxdecimal.Numeric `db:"maximum"`
-	CycleTypeID      *int                `db:"cycle_type_id"`
-	Rollover         bool                `db:"rollover"`
-	JointUserID      *pgtype.UUID        `db:"joint_user_id"`
-	CreatedAt        time.Time           `db:"created_at"`
-	UpdatedAt        time.Time           `db:"updated_at"`
+	ID               uuid.UUID  `db:"id"`
+	UserID           uuid.UUID  `db:"user_id"`
+	Name             string     `db:"name"`
+	ParentCategoryID *uuid.UUID `db:"parent_category_id"`
+	Maximum          *float64   `db:"maximum"`
+	CycleTypeID      *int       `db:"cycle_type_id"`
+	Rollover         bool       `db:"rollover"`
+	JointUserID      *uuid.UUID `db:"joint_user_id"`
+	CreatedAt        time.Time  `db:"created_at"`
+	UpdatedAt        time.Time  `db:"updated_at"`
 }
 
 type CategoryCreateRequest struct {
-	UserID           pgtype.UUID         `db:"user_id"`
-	Name             string              `db:"name"`
-	ParentCategoryID *pgtype.UUID        `db:"parent_category_id"`
-	Maximum          *pgxdecimal.Numeric `db:"maximum"`
-	CycleTypeID      *int                `db:"cycle_type_id"`
-	Rollover         bool                `db:"rollover"`
-	JointUserID      *pgtype.UUID        `db:"joint_user_id"`
+	UserID           uuid.UUID  `db:"user_id"`
+	Name             string     `db:"name"`
+	ParentCategoryID *uuid.UUID `db:"parent_category_id"`
+	Maximum          *float64   `db:"maximum"`
+	CycleTypeID      *int       `db:"cycle_type_id"`
+	Rollover         bool       `db:"rollover"`
+	JointUserID      *uuid.UUID `db:"joint_user_id"`
 }
 
 type CategoryUpdateRequest struct {
-	ID               pgtype.UUID         `db:"id"`
-	Name             string              `db:"name"`
-	ParentCategoryId *pgtype.UUID        `db:"parent_category_id"`
-	Maximum          *pgxdecimal.Numeric `db:"maximum"`
-	Rollover         bool                `db:"rollover"`
-	JointUserId      *pgtype.UUID        `db:"joint_user_id"`
+	ID               uuid.UUID  `db:"id"`
+	Name             string     `db:"name"`
+	ParentCategoryId *uuid.UUID `db:"parent_category_id"`
+	Maximum          float64    `db:"maximum"`
+	Rollover         bool       `db:"rollover"`
+	JointUserId      *uuid.UUID `db:"joint_user_id"`
 }
 
-func (repo *Repository) GetCategory(ctx context.Context, id pgtype.UUID) (*Category, error) {
+func (repo *Repository) GetCategory(ctx context.Context, id uuid.UUID) (*Category, error) {
 	result := &Category{}
 	statement := `
 		SELECT id, user_id, name, parent_category_id, maximum, cycle_type_id, rollover, joint_user_id, created_at, updated_at
 		FROM categories
 		WHERE id = $1;
 	`
-	err := repo.Pool.GetContext(ctx, result, statement, helpers.UUIDToString(id))
+	err := repo.Pool.GetContext(ctx, result, statement, id.String())
 	if err != nil {
 		return nil, err
 	}
@@ -81,7 +79,7 @@ func (repo *Repository) CreateCategory(ctx context.Context, category *CategoryCr
 		category.UserID, category.Name, category.ParentCategoryID, category.Maximum, category.CycleTypeID, category.Rollover, category.JointUserID,
 	).StructScan(result)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create category for user %s: %w", helpers.UUIDToString(category.UserID), err)
+		return nil, fmt.Errorf("failed to create category for user %s: %w", category.UserID.String(), err)
 	}
 	return result, nil
 }
